c53ba22bdef0d4bd275c04491db62882cdaaded8
pjotermartwica <piotrmartwicki43@gmail.com>
2026-04-05 14:56:30 +0200

    release: 1.0.10 screen adaptation config priority fix

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ sys.excepthook = _excepthook
 import openpyxl
 from dotenv import dotenv_values
 
-__version__ = "1.0.9"
+__version__ = "1.0.10"
 GITHUB_REPO = "pjotermartwica/ShiftFlow"
 
 def _collect_env_paths():
